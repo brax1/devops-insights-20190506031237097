7b8bc04e69dd9b96bab838fe1b2ece84998244ca
Braxton Ah Chee <braxton.ahchee@hotmail.com>
2019-05-16 01:01:21 +0000

    testing

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -94,7 +94,7 @@
 
 			var body = {
 				cod: 200,
-				name: 'El Paso',
+				name: 'Hamilton',
 				weather: [{
 					main: 'cold'
 				}],
@@ -115,7 +115,7 @@
 			apiv1.getWeather(reqMock, resMock);
 
 			assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
-			assert(resMock.send.lastCall.args[0].city === 'El Paso', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
+			assert(resMock.send.lastCall.args[0].city === 'Hamilton', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
 			assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
 		});
 	});
