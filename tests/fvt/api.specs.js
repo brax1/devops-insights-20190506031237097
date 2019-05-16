bf35a7fa3482a1b44b786529f93923919ca6f1b8
Braxton Ah Chee <braxton.ahchee@hotmail.com>
2019-05-16 02:51:52 +0000

    Testing

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -22,7 +22,7 @@
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?&q=Hamilton,nz'
+              url: appUrl + '/api/v1/getWeather?zip=hamilton,nz'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
@@ -60,7 +60,7 @@
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?&q=Rotorua,nz'
+              url: appUrl + '/api/v1/getWeather?zip=rotorua,nz'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
